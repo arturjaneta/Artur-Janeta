b1c812d6bc0c8770816c7e2607816b0dc3c965c3
arturjaneta <32541851+arturjaneta@users.noreply.github.com>
2017-11-14 18:03:11 +0100

    Rename ConsoleApplication1.cpp to insertsort.cpp

---
+++ ConsoleApplication1/ConsoleApplication1/insertsort.cpp
@@ -0,0 +1,35 @@
+﻿// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+void insertSort(float tab[], int size)
+{
+	int i, j, value;
+
+	for (i = 1; i < size; ++i)
+	{
+		value = tab[i];
+		for (j = i - 1; j >= 0 && tab[j] > value; --j)
+		{
+			tab[j + 1] = tab[j];
+		}
+		tab[j + 1] = value;
+	}
+}
+
+int main()
+{
+	float tab[5] = { 1, 3, -55, -1, 2 };
+	for (int i = 0; i < 5; i++) {
+		std::cout << tab[i] << "  ";
+	}
+	std::cout << std::endl;
+	insertSort(tab, 5);
+	for (int i = 0; i < 5; i++) {
+	std::cout << tab[i] << "  ";
+	}
+    return 0;
+}
+
